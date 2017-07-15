e9683b565789b1babe51bc3a4ab69a1144c8aabb
marciahon29 <marcia.hon.29@gmail.com>
2017-07-15 17:49:12 -0400

    Add files via upload

---
+++ VGG16_Experiment/Scripts/bottleneck_runall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python VGG16_bottleneck_data_00.py > accuracy_d00_e140_b40.txt
+python VGG16_bottleneck_data_01.py > accuracy_d01_e140_b40.txt
+python VGG16_bottleneck_data_02.py > accuracy_d02_e140_b40.txt
+python VGG16_bottleneck_data_03.py > accuracy_d03_e140_b40.txt
+python VGG16_bottleneck_data_04.py > accuracy_d04_e140_b40.txt
