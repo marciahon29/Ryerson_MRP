8a787425cf65404f869026a0b16482eafc3b813d
marciahon29 <marcia.hon.29@gmail.com>
2017-07-15 18:36:00 -0400

    Delete bottleneck_runall.sh

--- InceptionV4_Experiment/Scripts/bottleneck_runall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python InceptionV4_bottle_00.py > accuracy_d00_e140_b40.txt
-python InceptionV4_bottle_01.py > accuracy_d01_e140_b40.txt
-python InceptionV4_bottle_02.py > accuracy_d02_e140_b40.txt
-python InceptionV4_bottle_03.py > accuracy_d03_e140_b40.txt
-python InceptionV4_bottle_04.py > accuracy_d04_e140_b40.txt
-
