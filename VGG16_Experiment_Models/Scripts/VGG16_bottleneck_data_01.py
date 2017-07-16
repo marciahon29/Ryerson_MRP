96fa7e329aa46830d6376854d143dead46757d92
marciahon29 <marcia.hon.29@gmail.com>
2017-07-15 21:14:34 -0400

    Add files via upload

---
+++ VGG16_Experiment_Models/Scripts/VGG16_bottleneck_data_01.py
@@ -0,0 +1,74 @@
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Dense
+from keras import applications
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+top_model_weights_path = 'bottleneck_VGG16_01.h5'
+train_data_dir = 'data_01/train'
+validation_data_dir = 'data_01/validation'
+nb_train_samples = 5120
+nb_validation_samples = 1280
+epochs = 100
+batch_size = 40
+
+
+def save_bottlebeck_features():
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, nb_train_samples // batch_size)
+    np.save(open('bottleneck_features_train.npy', 'w'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, nb_validation_samples // batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'w'),
+            bottleneck_features_validation)
+
+
+def train_top_model():
+    train_data = np.load(open('bottleneck_features_train.npy'))
+    train_labels = np.array(
+        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
+
+    validation_data = np.load(open('bottleneck_features_validation.npy'))
+    validation_labels = np.array(
+        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+
+    model = Sequential()
+    model.add(Flatten(input_shape=train_data.shape[1:]))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='rmsprop',
+                  loss='binary_crossentropy', metrics=['accuracy'])
+
+    model.fit(train_data, train_labels,
+              epochs=epochs,
+              batch_size=batch_size,
+              validation_data=(validation_data, validation_labels))
+    model.save(top_model_weights_path)
+
+
+save_bottlebeck_features()
+train_top_model()
---
+++ VGG16_Experiment_Models/Scripts/bottleneck_runall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python VGG16_bottleneck_data_00.py > accuracy_d00_e100_b40.txt
+python VGG16_bottleneck_data_01.py > accuracy_d01_e100_b40.txt
+python VGG16_bottleneck_data_02.py > accuracy_d02_e100_b40.txt
+python VGG16_bottleneck_data_03.py > accuracy_d03_e100_b40.txt
+python VGG16_bottleneck_data_04.py > accuracy_d04_e100_b40.txt
