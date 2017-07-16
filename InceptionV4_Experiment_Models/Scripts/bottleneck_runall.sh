4e40aaa0dabe5ee978c332ab2886a22df999fe47
marciahon29 <marcia.hon.29@gmail.com>
2017-07-15 21:23:17 -0400

    Add files via upload

---
+++ InceptionV4_Experiment_Models/Scripts/bottleneck_runall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python InceptionV4_bottle_00.py > accuracy_iv4_d00_e100_b40.txt
+python InceptionV4_bottle_01.py > accuracy_iv4_d01_e100_b40.txt
+python InceptionV4_bottle_02.py > accuracy_iv4_d02_e100_b40.txt
+python InceptionV4_bottle_03.py > accuracy_iv4_d03_e100_b40.txt
+python InceptionV4_bottle_04.py > accuracy_iv4_d04_e100_b40.txt
+
